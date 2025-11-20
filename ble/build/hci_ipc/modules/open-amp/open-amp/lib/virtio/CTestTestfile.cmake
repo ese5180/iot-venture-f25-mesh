12265d2449782f907da8a99ef41bb428098c9af9
Haichao Zhao <haichao@seas.upenn.edu>
2025-11-20 18:05:55 -0500

    Integrate Memfault

---
+++ ble/build/hci_ipc/modules/open-amp/open-amp/lib/virtio/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /opt/nordic/ncs/v3.0.2/modules/lib/open-amp/open-amp/lib/virtio
+# Build directory: /Users/zhaohaichao/Desktop/ESE5180/iot-venture-f25-mesh/ble/build/hci_ipc/modules/open-amp/open-amp/lib/virtio
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
