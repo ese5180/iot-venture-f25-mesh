e8f3cb70fa89423e166cc81ff26ea334fadae46b
nnnniiicce <xinyi888@seas.upenn.edu>
2025-11-23 18:15:27 -0500

    Delete ble/build directory

--- ble/build/ble/modules/open-amp/open-amp/lib/utils/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/nordic/ncs/v3.0.2/modules/lib/open-amp/open-amp/lib/utils
-# Build directory: /Users/zhaohaichao/Desktop/ESE5180/iot-venture-f25-mesh/ble/build/ble/modules/open-amp/open-amp/lib/utils
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
