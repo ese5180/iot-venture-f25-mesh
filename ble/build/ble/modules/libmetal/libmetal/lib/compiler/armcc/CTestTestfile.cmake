2769c81ae09c18b01e2e30698d4e42e43d12ac4f
Haichao Zhao <haichao@seas.upenn.edu>
2025-11-23 20:58:17 -0500

    Fix Web BLE Communicate

--- ble/build/ble/modules/libmetal/libmetal/lib/compiler/armcc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/nordic/ncs/v3.0.2/modules/hal/libmetal/libmetal/lib/compiler/armcc
-# Build directory: /Users/zhaohaichao/Desktop/ESE5180/iot-venture-f25-mesh/ble/build/ble/modules/libmetal/libmetal/lib/compiler/armcc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ble/build/hci_ipc/modules/open-amp/open-amp/lib/remoteproc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/nordic/ncs/v3.0.2/modules/lib/open-amp/open-amp/lib/remoteproc
-# Build directory: /Users/zhaohaichao/Desktop/ESE5180/iot-venture-f25-mesh/ble/build/hci_ipc/modules/open-amp/open-amp/lib/remoteproc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
